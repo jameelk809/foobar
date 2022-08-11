005582a35bd74d9f25af5b78094f94374d05ba6e
Kamran Jameel <jameelk809@gmail.com>
2022-08-12 00:18:03 +0530

    files added

---
+++ LeetCode/Strings/1678 Goal Parser Interpretation.py
@@ -0,0 +1,8 @@
+def interpret(command):
+    command = command.replace("()", "o")
+    command = command.replace("(al)", "al")
+    return command
+
+
+command = "G()()()()(al)"
+print(interpret(command))
